c1ea98cb89295ed1c61e07f0a7f44b96014a8cb7
ShahMinulAmin <sajib.iosdev@gmail.com>
2019-03-18 01:58:21 +0600

    Updated integration tests of route search rest api

--- src/test/java/com/sajib/graph/web/integration/test/SearchControllerIT.java
+++ src/test/java/com/sajib/graph/web/integration/test/SearchControllerIT.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,16 +38,13 @@ public class SearchControllerIT {
 
     @Test
     public void SearchParams1_SearchRoute_SearchResult() throws Exception {
-        SearchController.SearchParams searchParams = new SearchController.SearchParams();
-        searchParams.from = "Vasastan";
-        searchParams.to = "Liseberg";
-        searchParams.transportTypes = new ArrayList<>();
-        searchParams.transportTypes.add("Road");
-        searchParams.container = 20;
-
         // Act
-        ResultActions result = mockMvc.perform(post("/api/v1/routes").contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.toJson(searchParams)));
+        ResultActions result = mockMvc.perform(get("/api/v1/routes")
+                .param(JsonUtil.PARAM_FROM, "Vasastan")
+                .param(JsonUtil.PARAM_TO, "Liseberg")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Road")
+                .param(JsonUtil.PARAM_CONTAINER, "20")
+                .contentType(MediaType.APPLICATION_JSON));
 
         // Assert
         result.andExpect(status().isOk())
@@ -57,17 +55,13 @@ public class SearchControllerIT {
 
     @Test
     public void SearchParams2_SearchRoute_SearchResult() throws Exception {
-
-        SearchController.SearchParams searchParams = new SearchController.SearchParams();
-        searchParams.from = "Himchari";
-        searchParams.to = "Banani";
-        searchParams.transportTypes = new ArrayList<>();
-        searchParams.transportTypes.add("Road");
-        searchParams.container = 20;
-
         // Act
-        ResultActions result = mockMvc.perform(post("/api/v1/routes").contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.toJson(searchParams)));
+        ResultActions result = mockMvc.perform(get("/api/v1/routes")
+                .param(JsonUtil.PARAM_FROM, "Himchari")
+                .param(JsonUtil.PARAM_TO, "Banani")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Road")
+                .param(JsonUtil.PARAM_CONTAINER, "20")
+                .contentType(MediaType.APPLICATION_JSON));
 
         // Assert
         result.andExpect(status().isOk())
@@ -76,27 +70,22 @@ public class SearchControllerIT {
                 .andExpect(jsonPath("$.routes[0].durationOfRoute", is(2)))
                 .andExpect(jsonPath("$.routes[1].costOfRoute", is(212)))
                 .andExpect(jsonPath("$.routes[1].durationOfRoute", is(2)));
-
     }
 
     @Test
     public void SearchParams3_SearchRoute_SearchResult() throws Exception {
-
-        SearchController.SearchParams searchParams = new SearchController.SearchParams();
-        searchParams.from = "Vasastan";
-        searchParams.to = "Maitland";
-        searchParams.transportTypes = new ArrayList<>();
-        searchParams.transportTypes.add("Road");
-        searchParams.transportTypes.add("Ocean");
-        searchParams.container = 20;
-        searchParams.minDays = 20;
-        searchParams.maxDays = 25;
-        searchParams.minCost = 2500;
-        searchParams.maxCost = 4000;
-
         // Act
-        ResultActions result = mockMvc.perform(post("/api/v1/routes").contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.toJson(searchParams)));
+        ResultActions result = mockMvc.perform(get("/api/v1/routes")
+                .param(JsonUtil.PARAM_FROM, "Vasastan")
+                .param(JsonUtil.PARAM_TO, "Maitland")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Road")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Ocean")
+                .param(JsonUtil.PARAM_CONTAINER, "20")
+                .param(JsonUtil.PARAM_MIN_DAYS, "20")
+                .param(JsonUtil.PARAM_MAX_DAYS, "25")
+                .param(JsonUtil.PARAM_MIN_COST, "2500")
+                .param(JsonUtil.PARAM_MAX_COST, "4000")
+                .contentType(MediaType.APPLICATION_JSON));
 
         // Assert
         result.andExpect(status().isOk())
@@ -112,18 +101,14 @@ public class SearchControllerIT {
 
     @Test
     public void SearchParams4_SearchRoute_SearchResult() throws Exception {
-
-        SearchController.SearchParams searchParams = new SearchController.SearchParams();
-        searchParams.from = "Liseberg";
-        searchParams.to = "Orlando";
-        searchParams.transportTypes = new ArrayList<>();
-        searchParams.transportTypes.add("Road");
-        searchParams.transportTypes.add("Ocean");
-        searchParams.container = 20;
-
         // Act
-        ResultActions result = mockMvc.perform(post("/api/v1/routes").contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.toJson(searchParams)));
+        ResultActions result = mockMvc.perform(get("/api/v1/routes")
+                .param(JsonUtil.PARAM_FROM, "Liseberg")
+                .param(JsonUtil.PARAM_TO, "Orlando")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Road")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Ocean")
+                .param(JsonUtil.PARAM_CONTAINER, "20")
+                .contentType(MediaType.APPLICATION_JSON));
 
         // Assert
         result.andExpect(status().isOk())
@@ -135,19 +120,15 @@ public class SearchControllerIT {
 
     @Test
     public void SearchParams5_SearchRoute_SearchResult() throws Exception {
-
-        SearchController.SearchParams searchParams = new SearchController.SearchParams();
-        searchParams.from = "Vasastan";
-        searchParams.to = "Maitland";
-        searchParams.transportTypes = new ArrayList<>();
-        searchParams.transportTypes.add("Road");
-        searchParams.transportTypes.add("Ocean");
-        searchParams.transportTypes.add("Air");
-        searchParams.container = 20;
-
         // Act
-        ResultActions result = mockMvc.perform(post("/api/v1/routes").contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.toJson(searchParams)));
+        ResultActions result = mockMvc.perform(get("/api/v1/routes")
+                .param(JsonUtil.PARAM_FROM, "Vasastan")
+                .param(JsonUtil.PARAM_TO, "Maitland")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Road")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Ocean")
+                .param(JsonUtil.PARAM_TRANSPORT_TYPES, "Air")
+                .param(JsonUtil.PARAM_CONTAINER, "20")
+                .contentType(MediaType.APPLICATION_JSON));
 
         // Assert
         result.andExpect(status().isOk())
@@ -161,16 +142,13 @@ public class SearchControllerIT {
 
     @Test
     public void MissingParam_SearchRoute_ReturnErrorMsg() throws Exception {
-        SearchController.SearchParams searchParams = new SearchController.SearchParams();
-        searchParams.from = "Vasastan";
-        searchParams.to = "Liseberg";
-
         // Act
-        ResultActions result = mockMvc.perform(post("/api/v1/routes").contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.toJson(searchParams)));
+        ResultActions result = mockMvc.perform(get("/api/v1/routes")
+                .param(JsonUtil.PARAM_FROM, "Vasastan")
+                .param(JsonUtil.PARAM_TO, "Liseberg")
+                .contentType(MediaType.APPLICATION_JSON));
 
         // Assert
-        result.andExpect(status().isOk())
-                .andExpect(jsonPath("$.errorMessage", is(JsonResponses.JSON_RESPONSE_MANDATORY_PARAM_MISSING)));
+        result.andExpect(status().isBadRequest());
     }
 }
